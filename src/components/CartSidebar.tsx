bff6fd3a62ab445ba6ba1ade397d62a68024d4c5
Milas2590 <cloudspicepvtltd@gmail.com>
2025-07-30 00:45:32 +0530

    Update CartSidebar.tsx

--- src/components/CartSidebar.tsx
+++ src/components/CartSidebar.tsx
@@ -33,7 +33,7 @@ export const CartSidebar = ({ isOpen, onClose }: CartSidebarProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 lg:hidden">
+    <div className="fixed inset-0 z-50">
       {/* Backdrop */}
       <div className="fixed inset-0 bg-black/50" onClick={onClose} />
       
